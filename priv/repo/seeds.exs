0df3cde3b3a8fc550d978ac63b7733ea8b4464af
Jonas Nygren <jonas_n_92@hotmail.com>
2021-07-20 19:26:42 +0200

    Added seeds for events

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,8 +12,15 @@
 
 alias WhackathonPlatform.Repo
 alias WhackathonPlatform.Users.User
+alias WhackathonPlatform.Event
 
 
 # Setting up some test users. One admin
 Repo.insert!(%User{password_hash: Pow.Ecto.Schema.Password.pbkdf2_hash("12345678"), password: "12345678", username: "anton", email: "testadmin@gmail.com", admin: true, bio: "Min första bio var lejonkungen"})
 Repo.insert!(%User{password_hash: Pow.Ecto.Schema.Password.pbkdf2_hash("12345678"), password: "12345678", username: "Testiluring", email: "derp@gmail.com", admin: false, bio: "Min första bio var Derpkungen"})
+
+# Set up some test events
+Repo.insert!(%Event{name: "Sommarhacket", theme: "Hawaii", description: "Knacka kod", start_time: DateTime.new!(~D[2021-07-16], ~T[16:00:00], "Etc/UTC"), end_time: DateTime.new!(~D[2021-07-17], ~T[16:00:00], "Etc/UTC")})
+Repo.insert!(%Event{name: "Julknacket", theme: "Tommy Körberg", description: "Gläns över sjö och strand", start_time: DateTime.new!(~D[2021-12-22], ~T[16:00:00], "Etc/UTC"), end_time: DateTime.new!(~D[2021-12-23], ~T[16:00:00], "Etc/UTC")})
+Repo.insert!(%Event{name: "Halloweensmacket", theme: "Pumpa läns", description: "Ordvitsar i halloweentider", start_time: DateTime.new!(~D[2021-10-30], ~T[16:00:00], "Etc/UTC"), end_time: DateTime.new!(~D[2021-10-31], ~T[16:00:00], "Etc/UTC")})
+Repo.insert!(%Event{name: "Telefonjacket", theme: "Erlang", description: "Det fantastiska språket som möjliggjorde denna sida", start_time: DateTime.new!(~D[2021-09-17], ~T[16:00:00], "Etc/UTC"), end_time: DateTime.new!(~D[2021-09-18], ~T[16:00:00], "Etc/UTC")})
