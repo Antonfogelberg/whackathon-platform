671691e14351352c3243792f0383123d2e36be5f
Jonas Nygren <jonas_n_92@hotmail.com>
2021-07-21 14:41:45 +0200

    Added start and end time to event pages and changed event description to have arbitrarily long text

--- lib/whackathon_platform_web/controllers/event_controller.ex
+++ lib/whackathon_platform_web/controllers/event_controller.ex
@@ -109,4 +109,31 @@ defmodule WhackathonPlatformWeb.EventController do
         "Sun"
     end
   end
+
+def get_formatted_time(time) do
+  case time do
+    0 ->
+      "00"
+    1 ->
+      "01"
+    2 ->
+      "02"
+    3 ->
+      "03"
+    4 ->
+      "04"
+    5 ->
+      "05"
+    6 ->
+      "06"
+    7 ->
+      "07"
+    8 ->
+      "08"
+    9 ->
+      "09"
+    _ ->
+      time
+  end
+end
 end
